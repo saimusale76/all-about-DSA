1875df31e1523ca2e4a9aa19511283630212ebd0
saimusale76 <saimusale76@gmail.com>
2023-01-23 22:17:14 +0530

    four commit

---
+++ pointer.cpp
@@ -0,0 +1,18 @@
+    #include <iostream>
+    using namespace std;
+    int main() { 
+         int i = 10;
+         
+         
+          int * p; 
+          
+          p = & i;
+            cout << "Address of the variable i is " << p << endl; 
+            cout << "Address of the pointer p is " << & p; 
+         
+
+
+
+
+    return 0;
+    }

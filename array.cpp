3674dc5998946698e89897bf0f8d88868134e089
saimusale76 <saimusale76@gmail.com>
2023-01-26 14:59:17 +0530

    fifth commit

---
+++ array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int row, col;
+    cout<<"no of rows"<<endl;
+    cin>>row;
+    cout<<"no of col"<<endl;
+    cin>>col;
+        int *ARR = new int[row*col];
+        for (int i = 0 ; i < row; i++)
+            for(int j = 0; j<col;j++)
+                *(ARR + i * col + j)= rand() % 100;
+         for (int i = 0 ; i < row; i++)
+            for(int j = 0; j<col;j++)
+                cout<<*(ARR + i * col + j) << " ";
+        
+        cout << endl;
+
+        delete[]ARR;
+        return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
